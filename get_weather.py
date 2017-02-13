e44b9184561c7c40e0aad73b8af56b629e29b64f
Ethan Wright <ethanwright@engr2-12-17-dhcp.int.colorado.edu>
2017-02-13 12:40:04 -0700

    Added time to get_weather

--- get_weather.py
+++ get_weather.py
@@ -1,21 +1,23 @@
 import requests
 import json
+import datetime
 #Documentation for API: https://darksky.net/dev/docs/response
 class Weather(object):
     def __init__(self):
         self.temp = 0        #The air temperature in degrees Fahrenheit.
         self.cloud_cover = 0  #The percentage of sky occluded by clouds, between 0 and 1, inclusive.
         self.wind_speed = 0     #The wind speed in miles per hour.
+        self.time = 0
   
     def refresh(self):
         url = 'https://api.forecast.io/forecast/52347449fab1dab5431fcbc264efcb19/40.014984,-105.270546'
-        f = requests.get(url).json()
-        current = f['currently']
-
+        data = requests.get(url).json()
+        current = data['currently']
         self.temp = current['temperature']
         self.cloud_cover = current['cloudCover']
         self.wind_speed = current['windSpeed']
-#        print current
+        self.time = current['time']
+
 
 
 myVar = Weather()
@@ -25,6 +27,9 @@ print myVar.temp, ' current temp'
 print myVar.cloud_cover * 100, 'percentage of cloud coverage'
 print myVar.wind_speed, ' MPH winds'
 
+print( datetime.datetime.fromtimestamp(int(myVar.time)).strftime('%Y-%m-%d %H:%M:%S'))
+time = datetime.datetime.fromtimestamp(int(myVar.time))
+print time.time()
 
 
 
