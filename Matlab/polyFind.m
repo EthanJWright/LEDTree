1546f079d7de1b26b4725c580a50973270e28c91
Ethan Wright <ethanwright@Ethans-MacBook-Air.local>
2017-02-13 21:36:32 -0700

    Fixed regression to look exactly how it should

--- Matlab/polyFind.m
+++ Matlab/polyFind.m
@@ -1,2 +1,2 @@
 function color = polyFind(wind, pixel_fit)
-color = pixel_fit(1) * (wind^3) + pixel_fit(2) * (wind^2) + pixel_fit(3)*(wind^3) + pixel_fit(4)
+color = pixel_fit(1) * (wind^2) + pixel_fit(2)*(wind) + pixel_fit(3)
--- Matlab/startClouds.m
+++ Matlab/startClouds.m
@@ -1,18 +1,19 @@
-wind = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+wind = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-red = [175, 186, 201, 219, 234, 220, 208, 188, 167, 139, 119]
-green = [206, 212, 223, 232, 242, 226, 212, 191, 170, 149, 128]
-blue = [255,255, 255, 255, 255, 237, 219, 196, 175, 165, 142]
+red = [176, 201, 221, 230, 240, 250, 255, 254, 240, 210, 200]
+green = [194, 215, 230 240, 250, 255, 255, 252, 240, 210, 200]
+%blue = [255,255, 255, 255, 255, 237, 219, 196, 175, 165, 142]
+blue = [255,255, 255, 255, 255,255,255, 255, 220, 210, 200]
+red_fit = polyfit(wind, red, 2)
+green_fit = polyfit(wind, green, 2)
+blue_fit = polyfit(wind, blue, 2)
 
-red_fit = polyfit(wind, red, 3)
-green_fit = polyfit(wind, green, 3)
-blue_fit = polyfit(wind, blue, 3)
 
 for i = 0:100
-    rgb = get_rgb((i/100), red_fit, green_fit, blue_fit)
-    r = rgb(1)/255
-    g = rgb(2)/255
-    b = rgb(3)/255
+    rgb = get_rgb(i, red_fit, green_fit, blue_fit)
+    r = (rgb(1) - 3)/255
+    g = (rgb(2) - 3)/255
+    b = (rgb(3) - 6)/255
     bar(1, 'FaceColor', [r g b])
     pause(.1)
 end
