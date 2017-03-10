e9d65b6716f863806b8e951328f65bc00a57f56d
Ethan James Wright <ewright362@gmail.com>
2017-03-10 13:32:03 -0700

    Some small changes

--- Live/demo_get_weather.py
+++ Live/demo_get_weather.py
@@ -2,7 +2,7 @@ import requests
 import json
 import datetime
 import os
-import time
+import time, threading
 #Powered by Dark Sky: https://darksky.net/forecast
 #Documentation for API: https://darksky.net/dev/docs/response
 class Weather(object):
@@ -11,33 +11,46 @@ class Weather(object):
         self.cloud_cover = 0  #The percentage of sky occluded by clouds, between 0 and 1, inclusive.
         self.wind_speed = 0     #The wind speed in miles per hour.
         self.time = 0
+        self.sun_up = 0
+        self.sun_down = 0
+
         self.api_key = '52347449fab1dab5431fcbc264efcb19'
         self.latitude = '40.014984'
         self.longitude = '-105.270546'
-        self.url = 'https://api.darksky.net/forecast/' + self.api_key + '/' + self.latitude + ',' + self.longitude
+
         self.data = requests.Session()
 
-
-    def is_json(self):
+    def test_connection(self):
        try:
-           self.data = requests.get(self.url).json()
+           url = 'https://api.darksky.net/forecast/' + self.api_key + '/' + self.latitude + ',' + self.longitude
+           self.data = requests.get(url).json()
        except requests.exceptions.RequestException as e:
-           print 'exception'
-           time.sleep(30)
+           print 'exception hit'
+           return False
+       except requests.exceptions.ValueError as e:
+           print 'Invalid JSON handled'
            return False
        return True
 
+    def convert_to_min(self, time):
+        minutes = int(datetime.datetime.fromtimestamp(int(time)).strftime('%M'))
+        hours = int(datetime.datetime.fromtimestamp(int(time)).strftime('%H'))
+        total = minutes + (hours * 60)
+        return total
+
+
     def refresh(self):
-        if(not self.is_json()):
+        if(not self.test_connection()):
+            time.sleep(30)
             self.refresh()
         current = self.data['currently']
         self.temp = current['temperature']
         self.cloud_cover = current['cloudCover'] * 100
         self.wind_speed = current['windSpeed'] * 10
-        self.time = int(datetime.datetime.fromtimestamp(int(current['time'])).strftime('%H'))
-
-myVar = Weather()
-myVar.refresh()
-print myVar.temp
-
-
+        self.time = self.convert_to_min(current['time'])
+        self.sun_up = self.convert_to_min(int(self.data['daily']['data'][4]['sunriseTime']))
+        self.sun_down = self.convert_to_min(int(self.data['daily']['data'][4]['sunsetTime']))
+        print self.sun_down
+        
+test = Weather()
+test.refresh()
